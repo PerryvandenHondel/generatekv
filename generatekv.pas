a6dd85f66386c2964381c06149bea990a6e8606c
Perry van den Hondel <perry.vandenhondel@gmail.com>
2017-05-17 15:43:05 +0200

    1.0.7 Writing to file

--- generatekv.pas
+++ generatekv.pas
@@ -9,8 +9,42 @@
 program GenerateKV;
 
 
+{$MODE OBJFPC}
+{$I+}
+
+
+uses
+	StrUtils,
+	SysUtils,
+	USupportLibrary;
+
+	
+var
+	path: AnsiString;
+	maxLines: integer;
+
+	
+procedure WriteToFile(path: AnsiString);
+var	
+	f: TextFile;
+	x: integer;
+begin
+	AssignFile(f, path);
+	{I+}
+	// Open the file in write mode.
+	ReWrite(f);
+	for x := 1 to maxLines do
+	begin
+		WriteLn(f, x);
+	end;
+	CloseFile(f);
+end;
+
+
 begin
 	WriteLn('Running...');
+	path := 'testkv.log';
+	WriteToFile(path)
 end. 
 
 // EOS
