b0b744f856fc00d23292271ba64a0d3b53f1eea8
Perry van den Hondel <perry.vandenhondel@gmail.com>
2017-05-17 15:31:25 +0200

    1.0.0 Initial setup

---
+++ generatekv.pas
@@ -0,0 +1,18 @@
+//
+// 	GenerateKV
+//
+//	Generate a log file with Key-Values for Splunk
+//
+//	Format: YYYY-MM-DD HH:MM:SS key=value key=value
+//
+
+program GenerateKV;
+
+
+begin
+	WriteLn('Running...');
+end. 
+
+// EOS
+
+
